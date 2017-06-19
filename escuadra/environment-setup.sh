07885555c31d826d6b3eb83ae71bd7fe5ae18448
Jaime Garcia <garciar.jaime@gmail.com>
2017-06-18 21:02:50 -0400

    Initial commit

---
+++ escuadra/environment-setup.sh
@@ -0,0 +1,3 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/
+export ANDROID_HOME=/Users/jgarciar/Library/Android/sdk
+export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH
